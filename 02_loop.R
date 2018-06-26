3cdcdf62d859c129b0a723c3e4c90d4bba588e8b
Matt Dray <mwdray@gmail.com>
2018-06-26 21:09:38 +0100

    add data read and correct loop

---
+++ 02_loop.R
@@ -0,0 +1,27 @@
+
+# Read data ---------------------------------------------------------------
+
+data <- read.csv(
+  file = "data/dawsons-creek-season-1.csv",
+  stringsAsFactors = FALSE
+)
+
+
+# Loop --------------------------------------------------------------------
+
+
+for (i in data$production_code){
+  
+  episode <- data[data$production_code == i, ]
+  
+  # the one-row dataframe object 'episode' we just created will be used as the
+  # source for filling the details in on the template
+  
+  # now render ('knit') the R Markdown file to Word format, name it and save it
+  
+  render(
+    "01_template.rmd",  # where to source the template from
+    output_file = paste0("episode_", i, ".docx"),  # name the output
+    output_dir = "output"  # folder to put the output file
+  )
+}
