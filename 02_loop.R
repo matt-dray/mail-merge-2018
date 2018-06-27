db8cabe137eaa8940d9e8eb73abde4643cf58e14
Matt Dray <mwdray@gmail.com>
2018-06-27 20:46:01 +0100

    complete loop script

--- 02_loop.R
+++ 02_loop.R
@@ -1,28 +1,41 @@
+# 'Mail Merge in 2018': automate report production with R
+# Read data and loop through to produce reports
+# Matt Dray
+# June 2018
 
-library(rmarkdown)
 
 # Read data ---------------------------------------------------------------
 
+
+# Read in the CSV data as a dataframe object
+# Each row is an episode of the first season of Dawson's Creek
+
 data <- read.csv(
-  file = "data/dawsons-creek-season-1.csv",
-  stringsAsFactors = FALSE
+  file = "data/dawsons-creek-season-1.csv",  # path to file
+  stringsAsFactors = FALSE  # read text columns as strings
 )
+
 
 # Loop --------------------------------------------------------------------
 
 
-for (i in data$production_code){
+library(rmarkdown)  # for render
+
+for (i in data$production_code){  # for each unique episode
   
-  episode <- data[data$production_code == i, ]
+  # Isolate that episode from the dataset
   
-  # the one-row dataframe object 'episode' we just created will be used as the
-  # source for filling the details in on the template
+  episode <- data[data$production_code == i, ]  
   
-  # now render ('knit') the R Markdown file to Word format, name it and save it
+  # The one-row dataframe object 'episode' we just created will be used as the
+  # source for filling the details in on the template file
+  
+  # Now render ('knit') the R Markdown file to Word format, name it and save it
   
   render(
-    "01_template.rmd",  # where to source the template from
+    input = "01_template.rmd",  # path to the template
     output_file = paste0("episode_", i, ".html"),  # name the output
-    output_dir = "output"  # folder to put the output file
+    output_dir = "output"  # folder in which to put the output file
   )
-}
+
+}  # end of loop
